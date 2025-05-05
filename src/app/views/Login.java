c04a64489e9fd3624a869b1035d908ef9b7ea41a
Uday Dhir <udaydhir794@gmail.com>
2025-05-05 13:10:51 +0530

    improved login page

--- src/app/views/Login.java
+++ src/app/views/Login.java
@@ -1,5 +1,6 @@
 package app.views;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,11 +11,14 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class Login {
+
+    static HashMap<String, String> users = new HashMap<>();
     @FXML
-    private TextField usernameField;
+    private TextField emailField;
     @FXML
     private PasswordField passwordField;
     @FXML
@@ -31,7 +35,7 @@ public class Login {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/views/HomePage.fxml"));
             root = loader.load();
             scene = new Scene(root);
-            stage = (Stage) usernameField.getScene().getWindow();
+            stage = (Stage) emailField.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
@@ -41,16 +45,30 @@ public class Login {
 
     @FXML
     private void handleLogin() {
-        String username = usernameField.getText();
+        String username = emailField.getText();
         String password = passwordField.getText();
 
         if (username.isEmpty() || password.isEmpty()) {
             messageBox.setText("Field cannot be empty!");
-        } else if (username.equals("admin") && password.equals("1234")) {
+        } else if (users.containsKey(username) && users.get(username).equals(password)) {
             messageBox.setText("Login Successful");
             loadHomePage();
         } else {
             messageBox.setText("Incorrect username or password");
+        }
+    }
+
+    public void handleCreateAccount() {
+        String username = emailField.getText();
+        String password = passwordField.getText();
+        if (users.containsKey(username))
+        {
+            messageBox.setText("Username is already taken!");
+        }
+        else
+        {
+            users.put(username,password);
+//            loadHomePage();
         }
     }
 }
